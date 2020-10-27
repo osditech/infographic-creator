04af4a5eb7f44634022f62cda5efe8873be9f2e2
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-10-27 11:57:48 +0530

    Core: download image functioality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as htmlToImage from 'html-to-image';
+import * as download from 'downloadjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  node;
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.node = this.myRef.current;
+    // console.log(node);
+    this.downloadImage();
+  }
+  downloadImage() {
+    // const node = this.myRef;
+    // console.log('App -> downloadImage -> node', node);
+
+    htmlToImage.toPng(this.node).then(dataUrl => {
+      download(dataUrl, 'sample-image.png');
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className='App' ref={this.myRef}>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        <div>
+          <h1>Download button</h1>
+          {/* <button onClick={this.downloadImage()}>download</button> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ImageDownloader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import App from './App';
+
+export class ImageDownloader extends Component {
+  render() {
+    return;
+  }
+}
+
+export default ImageDownloader;
